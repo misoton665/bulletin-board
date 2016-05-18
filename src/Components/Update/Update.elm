493592e578e7f7adde35db02fad80fe56c811a38
misoton665 <misoton998@hotmail.co.jp>
2016-05-18 16:15:03 +0900

    pattern matching error with imported modules

--- src/Components/Update/Update.elm
+++ src/Components/Update/Update.elm
@@ -1,12 +1,20 @@
-module Components.Update.Update exposing (Message(..), update)
+module Components.Update.Update exposing (Message, update)
 
 import Components.Model.Model as M
 
--- UPDATE
-type Message = NoOp | Increment
+type Page = Home | RegisterNewUser | UserProfile
 
-update : Message -> M.Model -> M.Model
+type Action a = PageTransition Page | LocalEvent a
+
+type NoEvent = NoEvent
+
+type alias TransitAction = Action NoEvent
+
+-- UPDATE
+type alias Message a = (Action a)
+
+update : Message a -> M.Model -> M.Model
 update msg (M.Model value) =
   case msg of
-    NoOp -> M.Model value
-    Increment -> M.Model <| value + 1
+    (PageTransition page) -> M.Model value
+    (LocalEvent NoEvent) -> M.Model <| value + 1
--- src/Components/View/Hello.elm
+++ src/Components/View/Hello.elm
@@ -4,11 +4,12 @@ import Html exposing (Html, div, text)
 import Html.Attributes exposing (class)
 import String
 
-import Components.Model.Model as M
+import Components.Model.Model exposing (..)
 
 -- hello component
-hello : M.Model -> Html a
-hello (M.Model value) =
-  div
-    [ class "mt-h2" ]
-    [ text ( "Hello, World" ++ ( String.repeat value <| "!" ) ) ]
+hello : Model -> Html a
+hello model = case model of
+  Model value ->
+    div
+      [ class "mt-h2" ]
+      [ text ( "Hello, World" ++ ( String.repeat value <| "!" ) ) ]
