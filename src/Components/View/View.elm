3be27c6bc0b8083fd7b977f1b4b28a4de4e58d06
misoton665 <misoton998@hotmail.co.jp>
2016-06-13 02:32:47 +0900

    Add comment field

--- src/Components/View/View.elm
+++ src/Components/View/View.elm
@@ -2,15 +2,14 @@ module Components.View.View exposing (view)
 
 import List as L
 
-import Html exposing (Html, div, p, text, button, img)
-import Html.Attributes exposing (class, style, src)
+import Html exposing (Html, div, p, text, button, img, input)
+import Html.Attributes exposing (class, style, src, type', placeholder)
 import Html.Events exposing ( onClick )
 
 import Components.Application.Comment as Comment exposing (Comment)
 import Components.Model.Model as M exposing (Model(..))
 import Components.Update.Update as U exposing (..)
 import Components.View.PageHeaderView exposing (pageHeader)
-import Components.View.Hello as Hello
 
 -- VIEW
 -- Examples of:
@@ -23,9 +22,16 @@ view (M.Model comments) =
     [ class "mt-palette-accent", style styles.wrapper ]
     [
       pageHeader <| Model comments,
+      commentField <| M.Model comments,
       button [ class "mt-button-sm", onClick <| U.Submission {author = "miso", body = "poe"} ] [ text "Submit!" ],
       Comment.toHtmlFromList comments
     ]
+
+commentField : M.Model -> Html (U.Message)
+commentField model = div [] [
+    input [type' "text", placeholder "author"] [],
+    input [type' "text", placeholder "comment"] []
+  ]
 
 -- CSS STYLES
 type alias Wrapper = List(String, String)
